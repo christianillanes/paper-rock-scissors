792fc4863acad818d234ea064071be54c851b568
Christian <christian.illanes@gmail.com>
2025-08-05 14:16:51 +0200

    Rearrange layout and add title

--- app/src/main/java/com/illanes/myapplication/MainActivity.kt
+++ app/src/main/java/com/illanes/myapplication/MainActivity.kt
@@ -4,13 +4,24 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.illanes.myapplication.ui.theme.MyApplicationTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +30,73 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             MyApplicationTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                App(modifier = Modifier)
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+fun App(modifier: Modifier = Modifier) {
+    Scaffold(modifier = modifier.fillMaxSize()) { innerPadding ->
+        Column (
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(innerPadding)
+        ) {
+            Text(
+                text = stringResource(id = R.string.app_name),
+                fontSize = 30.sp,
+                modifier = Modifier
+                    .align(alignment = Alignment.CenterHorizontally)
+            )
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceEvenly
+            ) {
+                Hand(
+                    modifier = Modifier
+                        .size(100.dp),
+                    handOption = HandOption.PAPER
+                )
+                Hand(
+                    modifier = Modifier
+                        .size(100.dp),
+                    handOption = HandOption.ROCK
+                )
+                Hand(
+                    modifier = Modifier
+                        .size(100.dp),
+                    handOption = HandOption.SCISSORS
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun Hand(
+    modifier: Modifier = Modifier,
+    handOption: HandOption = HandOption.PAPER
+) {
+    val handIcon = when(handOption) {
+        HandOption.PAPER -> R.drawable.paper
+        HandOption.ROCK -> R.drawable.rock
+        HandOption.SCISSORS -> R.drawable.scissors
+    }
+
+    Image(
+        painter = painterResource(id = handIcon),
+        contentDescription = stringResource(id = R.string.rock),
         modifier = modifier
     )
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun TitlePreview() {
     MyApplicationTheme {
-        Greeting("Android")
+        App()
     }
 }
